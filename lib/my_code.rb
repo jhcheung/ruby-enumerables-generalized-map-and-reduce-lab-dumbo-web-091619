cccce7aa5317fa2338f9f984e88554922aaa3b6c
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-09-13 17:36:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
 # Your Code Here
+def map(array)
+  new_array = []
+  for i in array do
+    new_array.push(yield(i))
+  end
+  new_array
+end 
+
+def reduce (array, starting_point = 0)
+  total = starting_point
+  for i in array do 
+    total += i 
+  end
+  total
+end 
